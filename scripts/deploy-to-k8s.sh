bc6a75bcf4f202279be4f9f4ad44604cee28e3fc
Santiago Gomez <Santiago.Gomez.Viamonte@ing.com>
2020-11-12 12:08:13 +0000

    scale lab completion

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
+
 kubectl apply -f deployment/pages-namespace.yaml
 kubectl apply -f deployment/pages-config.yaml
 kubectl apply -f deployment/pages-service.yaml
